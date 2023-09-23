6185b4be44a38a4f9c74f25a9abffe83c77c2c52
luisocasio <luopscode@gmail.com>
2023-09-23 16:51:40 -0600

    added Allow to headers

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.USER_REGISTRATION_SERVER,
   headers: {
     "Content-Type": "application/json",
+    "Allow": ["GET", "POST", "HEAD"]
   },
   withCredentials: true,
 });
