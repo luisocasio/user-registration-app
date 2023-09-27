ed1405ef830a0b7d9cf79234482a7b1d69d7506e
luisocasio <luopscode@gmail.com>
2023-09-27 14:14:10 -0600

    update

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: `http://localhost:8081`,
   headers: {
-    "Access-Control-Allow-Origin" : "*://*",
+    "Access-Control-Allow-Origin" : "*",
     "Access-Control-Allow-Methods": "POST, GET, PUT",
   },
   withCredentials: true,
