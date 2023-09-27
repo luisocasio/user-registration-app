e7aa302fe8ea11e319976f067c723386059aa964
luisocasio <luopscode@gmail.com>
2023-09-26 19:30:47 -0600

    add origin to headers

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -4,7 +4,8 @@ const axiosInstance = axios.create({
   baseURL: process.env.USER_REGISTRATION_SERVER,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Methods": "*"
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Origin" : "*",
   },
   withCredentials: true,
 });
