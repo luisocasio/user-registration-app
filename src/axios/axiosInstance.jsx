0a4465d2a1639e9006332750f637e26c026fd440
luisocasio <luopscode@gmail.com>
2023-09-28 20:53:27 -0600

    added server url

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:8081`,
+  baseURL: `https://user-registration-server.onrender.com`,
   headers: {
     "Access-Control-Allow-Origin" : "*",
     "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
