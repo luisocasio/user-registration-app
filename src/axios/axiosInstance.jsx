0205c2bc92246702cd7e9721a9b36ad6e0ae9c69
luisocasio <luopscode@gmail.com>
2023-09-28 21:12:57 -0600

    test cors

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: `https://user-registration-server.onrender.com`,
   headers: {
-    "Access-Control-Allow-Origin" : "http://localhost:3001, https://user-registration-entc5q4xp-fullstackog.vercel.app/login",
+    "Access-Control-Allow-Origin" : "*",
     "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
     'Content-Type': 'application/json',
     "Access-Control-Expose-Headers": "*"
