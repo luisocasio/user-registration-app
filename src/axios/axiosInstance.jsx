31e0447787916ca285edae7518ffa6ceb8cb3556
luisocasio <luopscode@gmail.com>
2023-09-28 21:03:26 -0600

    resolve cors

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: `https://user-registration-server.onrender.com`,
   headers: {
-    "Access-Control-Allow-Origin" : "*",
+    "Access-Control-Allow-Origin" : "http://localhost:3001",
     "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
     'Content-Type': 'application/json',
     "Access-Control-Expose-Headers": "*"
